3d9292fc2237e80ec8b3e337e3ae63a967a87aa7
Mariaorostegui <mfop001new@gmail.com>
2023-06-16 09:50:25 -0500

    ultimos Cambios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//Visualizar data
+
+const pokemonData=data.pokemon;
+
+document.body.onload=addElement;
+
+function addElement(){
+
+  for(const pokemon of pokemonData) {
+
+    const pokemonCard=document.createElement('div'),
+      cardContent=document.createTextNode(pokemon['num']),
+      cardImage=document.createElement('img');
+    cardImage.setAttribute("src", pokemon['img']);
+    cardImage.classList.add("images")
+    pokemonCard.classList.add("box");
+    
+    pokemonCard.appendChild(cardContent);
+    document.getElementById("container-pokemon").appendChild(pokemonCard); 
+    pokemonCard.appendChild(cardImage)
+  }
+}
