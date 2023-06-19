d84478cc254222be5c9e8756b084280a8e34aa15
AndressaSRodrigues <srandressa2@gmail.com>
2023-06-19 16:19:48 -0400

    intentos

--- src/data.js
+++ src/data.js
@@ -1,6 +1,17 @@
-export const filterData = (data, pokemonType)=> {
+export const filterType = (data, pokemonType)=> {
 
   const eachType = data.filter(pokemon => pokemon['type'][0] === pokemonType || pokemon['type'][1] === pokemonType )
   console.log(eachType);
   return eachType;
+}
+
+export const filterResistance = (data, pokemonResistance)=> {
+
+  const eachResistance = pokemon.resistant;
+
+  for (let i=0; eachResistance.length < i; i++) {
+    data.filter(eachResistance) === pokemonResistance;
+  }
+  console.log(eachResistance);
+  return eachResistance;
 }
