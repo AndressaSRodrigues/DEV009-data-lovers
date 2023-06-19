bc39bcdc15960dd5d3c873e9a239ab5220186b9d
AndressaSRodrigues <srandressa2@gmail.com>
2023-06-19 14:33:56 -0400

    Select event listener

--- src/main.js
+++ src/main.js
@@ -1,45 +1,40 @@
 import data from './data/pokemon/pokemon.js';
-import { filterTypes } from './data.js';
+import { filterData } from './data.js';
 
-//Visualizar data
-
-const pokemonData=data.pokemon;
-
-document.addEventListener('load', addElement(pokemonData))
+const pokemonData= data.pokemon;
 
 function addElement(importedData){
 
   for(const pokemon of importedData) {
 
     const pokemonCard=document.createElement('div'),
-      cardName=document.createTextNode(pokemon['name']),
-      cardType=document.createTextNode(pokemon['type']),
-      cardNameStyle=document.createElement('span'),
-      cardTypeStyle=document.createElement('span'),
+    cardName=document.createTextNode(pokemon['name']),
+    cardType=document.createTextNode(pokemon['type']),
+    cardNameStyle=document.createElement('span'),
+    cardTypeStyle=document.createElement('span'),
+    cardImage=document.createElement('img');
 
-      cardImage=document.createElement('img');
-      cardImage.setAttribute("src", pokemon['img']);
-      cardImage.classList.add("images");
-      pokemonCard.classList.add("box");
-      cardNameStyle.classList.add("name-style");
-      cardTypeStyle.classList.add("type-style");
+    cardImage.setAttribute("src", pokemon['img']);
+    cardImage.classList.add("images");
+    pokemonCard.classList.add("box");
+    cardNameStyle.classList.add("name-style");
+    cardTypeStyle.classList.add("type-style");
 
-      pokemonCard.classList.add(pokemon['type'][0]);
+    pokemonCard.classList.add(pokemon['type'][0]);
 
-      pokemonCard.appendChild(cardNameStyle);
-      pokemonCard.appendChild(cardImage);
-      pokemonCard.appendChild(cardTypeStyle);
-      cardNameStyle.appendChild(cardName);
-      cardTypeStyle.appendChild(cardType);
-      document.getElementById("container-pokemon").appendChild(pokemonCard); 
+    pokemonCard.appendChild(cardNameStyle);
+    pokemonCard.appendChild(cardImage);
+    pokemonCard.appendChild(cardTypeStyle);
+    cardNameStyle.appendChild(cardName);
+    cardTypeStyle.appendChild(cardType);
+    document.getElementById("container-pokemon").appendChild(pokemonCard); 
     }
 };
 
-//Checkboxes for types
-const checkboxes = document.querySelectorAll('.poke-type');
+addElement(pokemonData)
 
-checkboxes.forEach(checkbox => {
-  checkbox.addEventListener('click', () => {
-    alert('Checkbox selected!')
+const pokemonTypes = document.getElementById('select-types');
+  pokemonTypes.addEventListener('change', (e) => {
+    console.log(pokemonTypes, e.target.value);
 });
-});
+
